a4295a4fdca898bc5f0efcc39d392a15dccb2a66
chriskaliX <chriskali@163.com>
2022-08-20 23:25:56 +0800

    temporary patch for windows

--- grpc/transfer/handler.go
+++ grpc/transfer/handler.go
@@ -117,7 +117,10 @@ func handleData(req *pb.RawData) {
 		fMessage["product"] = req.Product
 
 		if dataType >= 100 && dataType <= 400 {
-			handler.ParseWinDataDispatch(fMessage, req, int(dataType))
+			for _, item := range req.Item {
+				// backport for windows for temp
+				handler.ParseWinDataDispatch(item.Fields, req, int(dataType))
+			}
 		} else {
 			switch dataType {
 			case 1:
@@ -131,56 +134,42 @@ func handleData(req *pb.RawData) {
 
 // 2021-10-28 增加心跳检测阈值判定
 func parseHeartBeat(hb map[string]string, req *pb.RawData) {
-
 	agentID := req.AgentID
 	conn, err := GlobalGRPCPool.Get(agentID)
 	if err != nil {
 		return
 	}
-
 	clearConn(conn)
-
-	strCPU, ok := hb["cpu"]
-	if ok {
+	if strCPU, ok := hb["cpu"]; ok {
 		if cpu, err := strconv.ParseFloat(strCPU, 64); err == nil {
 			conn.Cpu = cpu
 		}
 	}
-
-	strIO, ok := hb["io"]
-	if ok {
+	if strIO, ok := hb["io"]; ok {
 		if io, err := strconv.ParseFloat(strIO, 64); err == nil {
 			conn.IO = io
 		}
 	}
-
-	strMem, ok := hb["memory"]
-	if ok {
+	if strMem, ok := hb["memory"]; ok {
 		if mem, err := strconv.ParseInt(strMem, 10, 64); err == nil {
 			conn.Memory = mem
 		}
 	}
-
-	strSlab, ok := hb["slab"]
-	if ok {
+	if strSlab, ok := hb["slab"]; ok {
 		if slab, err := strconv.ParseInt(strSlab, 10, 64); err == nil {
 			conn.Slab = slab
 		}
 	}
-
 	conn.HostName = req.Hostname
 	conn.Version = req.Version
 	if req.IntranetIPv4 != nil {
 		conn.IntranetIPv4 = req.IntranetIPv4
 	}
-
 	if req.IntranetIPv6 != nil {
 		conn.IntranetIPv6 = req.IntranetIPv6
 	}
-
 	//last heartbeat time get from server
 	conn.LastHeartBeatTime = time.Now().Unix()
-
 	// 传输时间检测
 	fmt.Printf("%f, %d\n", conn.Cpu, conn.Memory)
 }
